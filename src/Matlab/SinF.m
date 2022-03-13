b4d2386a9f206a0cddebb01487cfc28f42599e9d
Chris <christoph_bothe@gmx.de>
2022-03-13 21:23:50 +0100

    Added SinF

--- src/Matlab/SinF.m
+++ src/Matlab/SinF.m
@@ -1,5 +1,33 @@
 function SinFOutput = SinF(input)
+    SinFOutput = single(zeros(size(input)));
+    for i = 1:numel(input)
 
-    SinFOutput = sin(input);
+        InputIsNegative = input(i) < 0;
+        input(i) = abs(input(i));
+        if input(i) > pi/2
+            input(i) = single(pi) - input(i);
+        end
 
+        c1 = single(-1.6665853e-1);
+        c2 = single(8.3142747e-3);
+        c3 = single(-1.8542223e-4);
+
+        InputPowTwo = input(i).*input(i);
+        InputPowThree = InputPowTwo.*input(i);
+        InputPowFive = InputPowThree.*InputPowTwo;
+
+        p1 = c3 * InputPowTwo;
+        p2 = p1 + c2;
+        p3 = c1 * InputPowThree;
+
+        p4 = p3 + input(i);
+        p5 = p2 .* InputPowFive;
+
+        p6 = p4 + p5;
+
+        if InputIsNegative
+           p6 = -1 * p6; 
+        end
+        SinFOutput(i) = p6;
+    end
 end
--- test/UnitTest/UnitTest1.m
+++ test/UnitTest/UnitTest1.m
@@ -1,12 +1,8 @@
 classdef UnitTest1 < matlab.unittest.TestCase
     methods(Test)
         function TestA(tc)
-            
-            x1 = single(rand()*1000);
-            
-            
-            
-            tc.verifyEqual(sin(x1),SinF(x1),'AbsTol',1e-4,'RelTol',1e-4)
+            x1 = single(rand(10000000,1)*2*pi-pi);
+            tc.verifyEqual(sin(x1),SinF(x1),'AbsTol',single(1.2e-6),'RelTol',single(1.2e-6))
         end
     end
 end
